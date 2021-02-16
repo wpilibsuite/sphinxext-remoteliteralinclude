d73d500529d42ce5c72550927f042b32a7179a8d
Daltz333 <daltzsmith@gmail.com>
2021-02-16 00:36:44 -0500

    Add Sphinx 2 testing compatability

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,12 +1,13 @@
 import pytest
 from sphinx.application import Sphinx
+from sphinx import version_info
 
 
 @pytest.mark.sphinx("html", testroot="simple-short")
 def test_simple_short(app: Sphinx):
     app.build()
 
-    content = (app.outdir / "index.html").read_text()
+    content = read_text(app)
 
     html = '<span class="n">Example</span> <span class="n">Domain</span>'
 
@@ -17,8 +18,14 @@ def test_simple_short(app: Sphinx):
 def test_simple_full(app: Sphinx):
     app.build()
 
-    content = (app.outdir / "index.html").read_text()
+    content = read_text(app)
 
     html = '<span class="n">Example</span> <span class="n">Domain</span>'
 
     assert html in content
+
+def read_text(app: Sphinx):
+    if version_info[:2] < (3, 0):
+        return (app.outdir / "index.html").text().replace("\n", "")
+    else:
+        return (app.outdir / "index.html").read_text()
