3f01271a817202472231bc75908668688a8bb37b
Madeline Scyphers <madelinescyphers@gmail.com>
2022-07-11 15:49:04 -0400

    Add a test for the pyobject to make sure it is working

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,7 +1,11 @@
 import pytest
 from sphinx.application import Sphinx
 from sphinx import version_info
+from sphinx.config import Config
+from sphinxext.remoteliteralinclude import RemoteLiteralIncludeReader
 
+
+DUMMY_CONFIG = Config({}, {})
 
 @pytest.mark.sphinx("html", testroot="simple-short")
 def test_simple_short(app: Sphinx):
@@ -23,6 +27,26 @@ def test_simple_full(app: Sphinx):
     html = '<span class="n">Example</span> <span class="n">Domain</span>'
 
     assert html in content
+
+
+def test_pyobject():
+    url = "https://raw.githubusercontent.com/wpilibsuite/sphinxext-remoteliteralinclude/main/sphinxext/remoteliteralinclude.py"
+    # Grab the entire RemoteLiteralIncludeReader.__init__
+    options = {'pyobject': 'RemoteLiteralIncludeReader.__init__'}
+    reader = RemoteLiteralIncludeReader(url, options, DUMMY_CONFIG)
+    content, lines = reader.read()
+
+    # only check the first line to be less susceptible to breaking with code changes
+    first_line = "    def __init__(self, url, options, config):"
+    assert content.splitlines()[0] == first_line
+
+    # we grab just the second line from `RemoteLiteralIncludeReader`
+    options = {"pyobject": "RemoteLiteralIncludeReader", "lines": "2-2"}
+    reader = RemoteLiteralIncludeReader(url, options, DUMMY_CONFIG)
+    content, lines = reader.read()
+
+    second_line = "    INVALID_OPTIONS_PAIR = [\n"  # this keeps \n because no splitlines
+    assert content == second_line
 
 
 def read_text(app: Sphinx):
