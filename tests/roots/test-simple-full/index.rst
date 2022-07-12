9566cc32cde3219a08637bb8cadf5734626eea7c
Madeline Scyphers <madelinescyphers@gmail.com>
2022-07-12 14:19:39 -0400

    Delete no longere needed files from test dir

--- tests/roots/test-simple-full/index.rst
+++
@@ -1,2 +0,0 @@
-.. rli:: http://example.com/
-   :lines: 4-4
--- tests/roots/test-simple-short/conf.py
+++
@@ -1,7 +0,0 @@
-extensions = ["sphinxext.remoteliteralinclude"]
-
-master_doc = "index"
-exclude_patterns = ["_build"]
-
-# removes most of the HTML
-html_theme = "basic"
